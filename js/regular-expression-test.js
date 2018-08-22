6b4381dfc0a048f33b3fbcb0baee6f42ae698c60
Surbhi Fotedar <surbhif@CESCHN.COM>
2018-08-22 15:23:39 +0530

    regular expression for date

--- js/regular-expression-test.js
+++ js/regular-expression-test.js
@@ -1,3 +1,15 @@
 (function() {
+  var quotedText = /'([^']*)'/;
+  console.log(quotedText.exec("she said 'hello'"));
   console.log(/abc/.test('supabc((t'));
+
+  function findDate(string) {
+    var dateTime = /(\d{1,2})-(\d{1,2})-(\d{4})/;
+    var match = dateTime.exec(string);
+    return new Date(Number(match[3]),
+      Number(match[2]) - 1,
+      Number(match[1]));
+  }
+  console.log(findDate("30-1-2003"));
+
 })();
